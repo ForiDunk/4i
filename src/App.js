95b77a1bae77380d1dce8f1808f382989cdb822d
bela-dev <fori-prg@pathwwway.com>
2019-01-17 18:32:51 +0200

    images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Credits from './components/Credits/Credits';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>This will be an awsome app!</h1>
+        <Credits />
       </div>
     );
   }
---
+++ src/components/Credits/Credits.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default () => (
+  <div>Icons made by 
+    <a 
+      href="https://www.freepik.com/" 
+      title="Freepik"> Freepik </a> 
+    from 
+    <a 
+      href="https://www.flaticon.com/" 
+      title="Flaticon"> www.flaticon.com </a> 
+    is licensed by 
+    <a 
+      href="https://creativecommons.org/licenses/by/3.0/" 
+      title="Creative Commons BY 3.0" 
+      rel="noopener noreferrer" 
+      target="_blank"> CC 3.0 BY </a>
+  </div>
+);
